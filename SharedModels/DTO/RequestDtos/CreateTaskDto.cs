f5373f52e7dc64e5f03fe03975aadc4b2b529da4
S.Penyaz <work.spenyaz@gmail.com>
2023-09-20 14:12:19 +0200

    DTO Initial.

---
+++ SharedModels/DTO/RequestDtos/CreateTaskDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharedModels.DTO.RequestDtos;
+
+public class CreateTaskDto
+{
+    [FromQuery(Name = "tittle")]
+    public string Tittle { get; set; } = string.Empty;
+
+    [FromQuery(Name = "description")]
+    public string Description { get; set; } = string.Empty;
+}
---
+++ SharedModels/DTO/ResponseDtos/ReadTaskDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedModels.DTO.ResponseDtos;
+
+public class ReadTaskDto
+{
+    public int Id { get; set; }
+    public string? Tittle { get; set; }
+    public string? Description { get; set; }
+}
